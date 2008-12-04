62b523e273cf0714ef223b7119032f9dd2861a27
Jonathan Viney <jonathan.viney@gmail.com>
2008-12-04 11:08:03 +0000

    Implement VAT calculations.

--- lib/default_attribute_value.rb
+++ lib/default_attribute_value.rb
@@ -7,10 +7,20 @@ module DefaultAttributeValue
   
   module ClassMethods
     def default_attribute_value(options)
-      options.each do |attribute, block|
+      options.each do |attribute, callback|
         define_method attribute do
           value = read_attribute(attribute)
-          value.present? ? value : block.call(self)
+          
+          if value.present?
+            value
+          else
+            case callback
+            when Symbol
+              send(callback)
+            when Proc
+              callback.call(self)
+            end
+          end
         end
       end
     end
